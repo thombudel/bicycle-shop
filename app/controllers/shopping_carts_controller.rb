443fa0f16dd58483ebe0209c33bee9de3581876e
Caroline Olijve <olijvecaroline@gmail.com>
2017-03-23 16:20:34 +0100

    shopping_cart controller methods added

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -1,3 +1,27 @@
+inlcude "ShoppingCart"
+
 class ShoppingCartsController < ApplicationController
-  
+set_cart
+
+def show
+  @cart = ShoppingCart.find(params[session[:shopping_cart]]
+end
+
+def create
+    @cart = current_cart
+    @cart.save
+    session[:shopping_cart]= @cart
+  end
+
+def update
+@cart= ShoppingCart.find(params[session[:shopping_cart]]
+#####
+end
+
+
+def destroy
+session[:shopping_cart] = nil
+end
+
+
 end
