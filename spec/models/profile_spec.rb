882ec11ed3e0f80f61eb5a4a47613cb4a93e7461
thom budel <thombudel@gmail.com>
2017-03-24 16:07:08 +0100

    minor styling and two model tests

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid withoutfirst name" do
+      profile = Profile.new(first_name: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:first_name)
+    end
+    it "is invalid without country" do
+      profile = Profile.new(country: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:country)
+    end
+  end
 end
